a50bf76b05cde853d23aa7068b905703567e112a
avi8230 <avi8230@gmail.com>
2025-03-12 02:34:52 +0200

    refactor: simplify App.vue by removing logout functionality and enhancing layout with Vuetify

--- movement-group-frontend/src/App.vue
+++ movement-group-frontend/src/App.vue
@@ -1,42 +1,9 @@
-<script setup lang="ts">
-import { useAuthStore } from './04-store/authStore';
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-const authStore = useAuthStore();
-
-// Function to log out the user
-const logoutUser = async () => {
-  await authStore.logout();
-  alert("Logged out successfully!");
-  router.push("/login");
-};
-</script>
-
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/register">Register</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <button @click="logoutUser">Logout</button>
-    </nav>
-
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <router-view />
+  </v-app>
 </template>
 
-<style scoped>
-nav {
-  display: flex;
-  gap: 15px;
-  margin-bottom: 20px;
-}
-button {
-  background: red;
-  color: white;
-  border: none;
-  padding: 5px;
-  cursor: pointer;
-}
-</style>
+<script lang="ts" setup>
+//
+</script>

de49ea0e46c1a8c763040490be054f79929401f5
avi8230 <avi8230@gmail.com>
2025-03-12 02:09:44 +0200

    Rename a folder "http"

---
+++ movement-group-frontend/src/http/authService.ts
@@ -0,0 +1,23 @@
+import axios from "./api";
+import type { Credentials } from "../types/Credentials";
+import type { User, RegisterUser } from "../types/User";
+
+const API_URL_AUTH = `${import.meta.env.VITE_API_URL}/auth`;
+
+const AuthService = {
+    register: async (user: RegisterUser): Promise<User> => {
+        const response = await axios.post<User>(`${API_URL_AUTH}/register`, user);
+        return response.data;
+    },
+
+    login: async (user: Credentials): Promise<User> => {
+        const response = await axios.post<User>(`${API_URL_AUTH}/login`, user);
+        return response.data;
+    },
+
+    logout: async (): Promise<void> => {
+        await axios.post(`${API_URL_AUTH}/logout`);
+    }
+};
+
+export default AuthService;
